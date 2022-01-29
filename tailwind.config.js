6df8f7c999b16fa87b6a7c8466a8c6cae3483683
Flye001 <philip.org1@gmail.com>
2022-01-29 21:06:26 +0000

    Made it look cool

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["*.html"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-black': '#171717',
+        'primary': '#e4e6ea',
+        'secondary': '#b1b3b7',
+        'line': '#3a3b3c',
+      },
+    },
   },
   plugins: [],
 }
